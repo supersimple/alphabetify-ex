ba856a65b3274e5989130aff8275cae059d4d56b
supersimple <toddresudek@gmail.com>
2017-01-02 17:39:24 -0700

    rolls over hashes

--- lib/alphabetify.ex
+++ lib/alphabetify.ex
@@ -3,41 +3,39 @@ defmodule Alphabetify do
   @hash_chars String.split("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "", trim: true) # builds an array with each character
 
   def generate_hash do
-    # traverse the last_hash from end to beginning. get_next_char for the last char. If that rolls over, get_next_char for the next-to-last char, etc. If all letters are rolled over, rollover_hash. write this to last_hash/1
     if Enum.uniq(String.split(last_hash, "", trim: true)) == [List.last(@hash_chars)] do
       rollover_hash(last_hash)
     else
-      last_hash.reverse
-      |> String.split("", trim: true)
-      |> Enum.each(fn(ch) -> ch end)
+      advance_hash(last_hash)
     end
-
   end
 
   def seed_hash(seed) do
     if String.length(seed) == 0, do: raise ArgumentError, message: "The seed cannot be empty"
     if Enum.all?(String.split(seed, "", trim: true), fn(ch) -> Enum.member?(@hash_chars, ch) end) do
       last_hash(seed)
+      last_hash
     else
       raise ArgumentError, message: "The seed can only contain characters in #{List.to_string(@hash_chars)}"
     end
   end
 
-  defp last_hash do
-    File.read!("last-hash.txt")
+  def last_hash do
+    File.read!(last_hash_file)
   end
 
   defp last_hash(str) do
-    File.write! "last-hash.txt", str
+    File.write! last_hash_file, str
   end
 
-  defp get_next_char(char) do
+  def get_next_char(char) do
     unless char == List.last(@hash_chars) do
-      Enum.find_index(hash_chars, fn(x) -> x == char end)
+      next_char = Enum.find_index(@hash_chars, fn(x) -> x == char end)
       |> Kernel.+(1)
-      |> [char_at, false]
+      |> char_at
+      next_char
     else
-      [List.first(@hash_chars), true]
+      List.first(@hash_chars)
     end
   end
 
@@ -50,6 +48,42 @@ defmodule Alphabetify do
     # roll it over and add one char (eg. ZZZ -> AAAA)
     List.first(@hash_chars)
     |> String.duplicate(String.length(hash) + 1)
+  end
+
+  defp advance_hash(last_hash) do
+    # Advance the last character
+    # If that char had to rollover, advance the next to last character
+    # repeat if necessary
+    # return the new string
+    # eg. AAZZ -> ABAA
+    # eg. AADZ -> AAEA
+    reversed_hash = String.reverse(last_hash)
+    |> String.split("", trim: true)
+
+    parts = Enum.split_while(reversed_hash, fn(ch) -> ch == List.last(@hash_chars) end)
+
+    rolled_hash = Tuple.to_list(parts) |> List.first |> Enum.map(fn(ch) -> get_next_char(ch) end)
+    advanced_char = Tuple.to_list(parts) |> List.last |> List.first |> get_next_char
+    unchanged_hash = Tuple.to_list(parts) |> List.last |> Enum.slice(1..-1)
+
+    Enum.concat(rolled_hash, [advanced_char])
+    |> Enum.concat(unchanged_hash)
+    |> Enum.join
+    |> String.reverse
+  end
+
+  def pop_last_member(enum) do
+    remaining_enum = Enum.slice(enum, 0..-2)
+    popped_value = Enum.at(enum, -1)
+    [remaining_enum, popped_value]
+  end
+
+  def last_hash_file do
+    if Mix.env == :test do
+      "last-hash-test.txt"
+    else
+      "last-hash.txt"
+    end
   end
 
 end
--- test/alphabetify_test.exs
+++ test/alphabetify_test.exs
@@ -2,7 +2,32 @@ defmodule AlphabetifyTest do
   use ExUnit.Case
   doctest Alphabetify
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "the seed hash returns the seed" do
+    assert "AAAA" == Alphabetify.seed_hash "AAAA"
+  end
+
+  test "the last hash returns the hash" do
+    File.write! Alphabetify.last_hash_file, "AAAA" #prepare the file for testing
+    assert "AAAA" == Alphabetify.last_hash
+  end
+
+  test "get next char" do
+    assert "D" == Alphabetify.get_next_char "C"
+    assert "A" == Alphabetify.get_next_char "Z"
+  end
+
+  test "generate hash" do
+    File.write! Alphabetify.last_hash_file, "ZZZZ" #prepare the file for testing
+    assert "AAAAA" == Alphabetify.generate_hash
+
+    File.write! Alphabetify.last_hash_file, "AAAA" #prepare the file for testing
+    assert "AAAB" == Alphabetify.generate_hash
+
+    File.write! Alphabetify.last_hash_file, "AAAZ" #prepare the file for testing
+    assert "AABA" == Alphabetify.generate_hash
+  end
+
+  test "pop_last_member" do
+    assert [["Z","Y","X"],"W"] == Alphabetify.pop_last_member(["Z","Y","X","W"])
   end
 end
