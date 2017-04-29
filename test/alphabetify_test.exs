b9cfda4b1fff368294badd1dbc6cce8d957b774f
supersimple <toddresudek@gmail.com>
2017-04-28 23:06:49 -0600

    WIP solidifying tests

--- test/alphabetify_test.exs
+++ test/alphabetify_test.exs
@@ -7,38 +7,10 @@ defmodule AlphabetifyTest do
   end
 
   test "the last hash returns the hash" do
-    {:ok, table} = :dets.open_file(:alphabetify_disk_test_store, [type: :set])
-    :dets.insert(table, {:last_hash, "AAAA"})
-    :dets.close(:alphabetify_disk_test_store)
     assert "AAAA" == Alphabetify.last_hash()
   end
 
   test "generate hash from AAAA" do
-    {:ok, table} = :dets.open_file(:alphabetify_disk_test_store, [type: :set])
-    :dets.insert(table, {:last_hash, "AAAA"})
-    :dets.close(:alphabetify_disk_test_store)
     assert "AAAB" == Alphabetify.generate_hash()
   end
-
-  test "generate hash from ZZZZ" do
-    {:ok, table} = :dets.open_file(:alphabetify_disk_test_store, [type: :set])
-    :dets.insert(table, {:last_hash, "ZZZZ"})
-    :dets.close(:alphabetify_disk_test_store)
-    assert "AAAAA" == Alphabetify.generate_hash()
-  end
-
-  test "generate hash from AAAZ" do
-    {:ok, table} = :dets.open_file(:alphabetify_disk_test_store, [type: :set])
-    :dets.insert(table, {:last_hash, "AAAZ"})
-    :dets.close(:alphabetify_disk_test_store)
-    assert "AABA" == Alphabetify.generate_hash()
-  end
-
-  test "generate hash from AZZZZZZZZ" do
-    {:ok, table} = :dets.open_file(:alphabetify_disk_test_store, [type: :set])
-    :dets.insert(table, {:last_hash, "AZZZZZZZZZ"})
-    :dets.close(:alphabetify_disk_test_store)
-    assert "BAAAAAAAAA" == Alphabetify.generate_hash()
-  end
-
 end
