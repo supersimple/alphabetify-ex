0b308ec4002737110808b59bfd958ea5b15af4ce
supersimple <toddresudek@gmail.com>
2018-07-05 13:46:12 -0600

    Increments version to 1.0.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Alphabetify.Mixfile do
   def project do
     [
       app: :alphabetify,
-      version: "1.0.8",
+      version: "1.0.9",
       elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
